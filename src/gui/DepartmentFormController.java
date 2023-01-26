27cbf93699716c3374e535527badd933827c3e52
Atila jr <atilla.junior33@gmail.com>
2023-01-26 17:50:32 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,17 +3,28 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentServices;
 
 public class DepartmentFormController implements Initializable {
 	
 	private Department entity;
+	
+	
+	private DepartmentServices service;
+	
+	
 
 	@FXML
 	private TextField txtId;
@@ -34,14 +45,44 @@ public class DepartmentFormController implements Initializable {
 		this.entity = entity;
 	}
 	
-	@FXML
-	public void onBtSaveAction() {
-		System.out.println("onBtSaveAction");
+	public void setDepartmentServices(DepartmentServices service) {
+		this.service = service;
 	}
 	
+	
 	@FXML
-	public void onBtCancelAction() {
-		System.out.println("onBtCancelAction");
+	public void onBtSaveAction(ActionEvent event) {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		
+		
+		try {
+		entity = getFormData();
+		service.saveOrUpdate(entity);
+		Utils.currentStage(event).close();
+		}
+		catch(DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+			
+		}
+	}
+	
+	private Department getFormData() {
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 	
 	@Override
--- src/model/services/DepartmentServices.java
+++ src/model/services/DepartmentServices.java
@@ -8,13 +8,18 @@ import model.entities.Department;
 
 public class DepartmentServices {
 
-	
 	private DepartmentDao dao = DaoFactory.createDepartmentDao();
-	
-	
-	public List<Department> findAll(){
-		
+
+	public List<Department> findAll() {
 		return dao.findAll();
 	}
-}
 	
+	public void saveOrUpdate(Department obj) {
+		if(obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+	}
+}
